7322cb49d13ca4b76016ac73979164138de9b17e
gabdepa <gabriel-r-p-p@hotmail.com>
2023-11-16 19:01:39 -0300

    changes

--- knn-mpi-1.sh
+++ knn-mpi-1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "Executando knn-mpi para $1 processos em $2 nodos."
-mpirun -N $2 -np $1 knn-mpi  128 400000 300 1024
+echo "Executando knn-mpi para $1 processos"
+mpirun -N $1 -np $1 knn-mpi  128 400000 300 1024
