67efc3278325e653ed6d2f3ed1ac6933d4ab4e93
gabdepa <gabriel-r-p-p@hotmail.com>
2023-11-16 18:25:53 -0300

    changes

--- knn-mpi-slurm.sh
+++ knn-mpi-slurm.sh
@@ -6,13 +6,11 @@ nodes=$(sinfo | awk 'NR==2{print $4}')
 # Agora a variável $nodes tem o valor do campo NODES
 echo "Número de nós disponíveis: $nodes"
 
-# mpirun -N 1 scatterTest
-
 # Rodar o programa para APENAS 1 processo MPI e medir o tempo da computaçao de knn
-mpirun -np 1 knn-mpi  128 400000 300 1024
+mpirun -N 1 -np 1 knn-mpi  128 400000 300 1024
 
 # Rodar o programa para 4 processos MPI no mesmo host e medir o tempo da computaçao de knn
-mpirun -np 4 knn-mpi  128 400000 300 1024
+mpirun -N 1 -np 4 knn-mpi  128 400000 300 1024
 
 # Rodar o programa para 4 processos MPI em hosts diferentes e medir o tempo da computaçao de knn
-mpirun -N $nodes -np 4 knn-mpi  128 400000 300 1024
+mpirun -N 1 -np 4 knn-mpi  128 400000 300 1024
