d4c513d632d2371dbd341300a0b00c4d1caf1d13
gabdepa <gabriel-r-p-p@hotmail.com>
2023-11-16 18:33:03 -0300

    changes

--- knn-mpi-slurm.sh
+++ knn-mpi-slurm.sh
@@ -7,10 +7,10 @@ nodes=$(sinfo | awk 'NR==2{print $4}')
 echo "Número de nós disponíveis: $nodes"
 
 # Rodar o programa para APENAS 1 processo MPI e medir o tempo da computaçao de knn
-mpirun -N 1 -np 1 knn-mpi  128 400000 300 1024
+sbatch --exclusive -N 1 mpirun -np 1 knn-mpi  128 400000 300 1024
 
 # Rodar o programa para 4 processos MPI no mesmo host e medir o tempo da computaçao de knn
-mpirun -N 1 -np 4 knn-mpi  128 400000 300 1024
+sbatch --exclusive -N 1 mpirun -np 4 knn-mpi  128 400000 300 1024
 
 # Rodar o programa para 4 processos MPI em hosts diferentes e medir o tempo da computaçao de knn
-mpirun -N 1 -np 4 knn-mpi  128 400000 300 1024
+sbatch --exclusive -N $nodes mpirun -np 4 knn-mpi  128 400000 300 1024
