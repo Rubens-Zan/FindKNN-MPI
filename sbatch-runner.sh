562a13579e52d3633a9cbe9bed537f1df7a09017
gabdepa <gabriel-r-p-p@hotmail.com>
2023-11-17 11:35:42 -0300

    Changes

--- sbatch-runner.sh
+++ sbatch-runner.sh
@@ -2,8 +2,6 @@
 
 # Roda o comando sinfo e usa awk para pegar o valor da coluna NODES
 nodes=$(sinfo | awk 'NR==2{print $4}')
-
-# Agora a variável $nodes tem o valor do campo NODES
 echo "> Número de nós disponíveis: $nodes"
 
 # Recebe número de processos desejado
