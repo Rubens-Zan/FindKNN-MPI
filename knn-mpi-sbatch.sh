e71986d727c317b76d124d6040ce6a958ca9a67a
gabdepa <gabriel-r-p-p@hotmail.com>
2023-11-17 10:18:25 -0300

    Changes

--- knn-mpi-sbatch.sh
+++ knn-mpi-sbatch.sh
@@ -11,12 +11,25 @@ make purge && make
 
 # Rodar o programa para APENAS 1 processo MPI e medir o tempo da computaçao de knn
 echo "Rodando sbatch --exclusive para 1 nodo..."
-sbatch --exclusive -N 1 knn-mpi-1.sh 1 
+# Iniciar a medição de tempo
+start_time=$(date +%s)
+sbatch --exclusive -N 1 run-knn-mpi 1 
+end_time=$(date +%s)
+echo "Tempo total de execução: $((end_time - start_time)) segundos"
+
 
 # Rodar o programa para 4 processos MPI no mesmo host e medir o tempo da computaçao de knn
 echo "Rodando sbatch --exclusive para 1 nodo..."
-sbatch --exclusive -N 1 knn-mpi-1.sh 4 
+# Iniciar a medição de tempo
+start_time=$(date +%s)
+sbatch --exclusive -N 1 run-knn-mpi 4 
+end_time=$(date +%s)
+echo "Tempo total de execução: $((end_time - start_time)) segundos"
 
 # Rodar o programa para 4 processos MPI em hosts diferentes e medir o tempo da computaçao de knn
-echo "Rodando sbatch --exclusive para 6 nodos..."
-sbatch --exclusive -N 4 knn-mpi-1.sh 4
+echo "Rodando sbatch --exclusive para 4 nodos..."
+# Iniciar a medição de tempo
+start_time=$(date +%s)
+sbatch ./run-knn-mpi 4
+end_time=$(date +%s)
+echo "Tempo total de execução: $((end_time - start_time)) segundos"
