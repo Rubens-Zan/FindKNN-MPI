388fcefad9b5e8829e2cf79cf6b2e80a596aaf9a
gabdepa <gabriel-r-p-p@hotmail.com>
2023-11-17 10:51:37 -0300

    Changes

--- knn-mpi-sbatch.sh
+++ knn-mpi-sbatch.sh
@@ -11,23 +11,32 @@ NUM_NODES=$1
 
 # Verifica se NUM_NODES é maior do que nodes
 if [ "$NUM_NODES" -le "$nodes" ]; then
-    echo "> Número de processos solicitado ($NUM_NODES) está dentro do limite de nós disponíveis ($nodes)."
+    echo "> Número de processos solicitado ($NUM_NODES) está dentro do limite de nós disponíveis ($nodes). Prosseguindo..."
+    
+    # Verifica se existem arquivos com padrão slurm* no diretório atual
+    if ls slurm* 1> /dev/null 2>&1; then
+        echo "> Removendo arquivos slurm antigos."
+        rm slurm*
+    else
+        echo "> Nenhum arquivo slurm antigo para remover. Prosseguindo...."
+    fi
 
-    # Remove antigos arquivos para que sejam criados novos
-    rm slurm*
     # Recompila arquivos de execução
-    make purge && make
-
+    echo "> Removendo antigos arquivos de compilação:"
+    make purge
+    echo "> Compilando arquivos para execução:"
+    make
+    
     # Rodar o programa para APENAS 1 processo MPI e medir o tempo da computaçao de knn
-    echo "> Rodando sbatch --exclusive para 1 nodo..."
+    echo "> Rodando sbatch --exclusive para 1 nodo:"
     sbatch --exclusive -N 1 knn-mpi-runner.sh 1
     
     # Rodar o programa para 4 processos MPI no mesmo host e medir o tempo da computaçao de knn
-    echo "> Rodando sbatch --exclusive para 1 nodo..."
+    echo "> Rodando sbatch --exclusive para 1 nodo:"
     sbatch --exclusive -N 1 knn-mpi-runner.sh $NUM_NODES
     
     # Rodar o programa para 4 processos MPI em hosts diferentes e medir o tempo da computaçao de knn
-    echo "> Rodando sbatch --exclusive para $NUM_NODES nodos..."
+    echo "> Rodando sbatch --exclusive para $NUM_NODES nodos:"
     sbatch --nodes=$NUM_NODES --ntasks-per-node=1 --exclusive knn-mpi-runner.sh $NUM_NODES
 else
     echo "ERROR: O número de processos solicitado ($NUM_NODES) é maior do que o número de nós disponíveis ($nodes)."
