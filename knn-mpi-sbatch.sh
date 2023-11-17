0c1d9e392dc16521192ba16a4b443f602a727d9c
gabdepa <gabriel-r-p-p@hotmail.com>
2023-11-17 10:44:04 -0300

    Changes

--- knn-mpi-sbatch.sh
+++ knn-mpi-sbatch.sh
@@ -13,22 +13,20 @@ echo "Número de nós disponíveis: $nodes"
 NUM_NODES=$1
 
 # Verifica se NUM_NODES é maior do que nodes
-if [ "$NUM_NODES" -gt "$nodes" ]; then
-    echo "O número de processos solicitado ($NUM_NODES) é maior do que o número de nós disponíveis ($nodes)."
-    # Aqui, você pode sair do script ou lidar com a situação conforme necessário
-    exit 1
+if [ "$NUM_NODES" -le "$nodes" ]; then
+    echo "> Número de processos solicitado ($NUM_NODES) está dentro do limite de nós disponíveis ($nodes)."
+
+    # Rodar o programa para APENAS 1 processo MPI e medir o tempo da computaçao de knn
+    echo "> Rodando sbatch --exclusive para 1 nodo..."
+    sbatch --exclusive -N 1 knn-mpi-runner.sh 1
+    
+    # Rodar o programa para 4 processos MPI no mesmo host e medir o tempo da computaçao de knn
+    echo "> Rodando sbatch --exclusive para 1 nodo..."
+    sbatch --exclusive -N 1 knn-mpi-runner.sh $NUM_NODES
+    
+    # Rodar o programa para 4 processos MPI em hosts diferentes e medir o tempo da computaçao de knn
+    echo "> Rodando sbatch --exclusive para $NUM_NODES nodos..."
+    sbatch --nodes=$NUM_NODES --ntasks-per-node=1 --exclusive knn-mpi-runner.sh $NUM_NODES
 else
-    echo "Número de processos solicitado ($NUM_NODES) está dentro do limite de nós disponíveis ($nodes)."
+    echo "ERROR: O número de processos solicitado ($NUM_NODES) é maior do que o número de nós disponíveis ($nodes)."
 fi
-
-# Rodar o programa para APENAS 1 processo MPI e medir o tempo da computaçao de knn
-echo "Rodando sbatch --exclusive para 1 nodo..."
-sbatch --exclusive -N 1 knn-mpi-runner.sh 1
-
-# Rodar o programa para 4 processos MPI no mesmo host e medir o tempo da computaçao de knn
-echo "Rodando sbatch --exclusive para 1 nodo..."
-sbatch --exclusive -N 1 knn-mpi-runner.sh $NUM_NODES 
-
-# Rodar o programa para 4 processos MPI em hosts diferentes e medir o tempo da computaçao de knn
-echo "Rodando sbatch --exclusive para $NUM_NODES nodos..."
-sbatch --nodes=$NUM_NODES --ntasks-per-node=1 --exclusive knn-mpi-runner.sh $NUM_NODES
