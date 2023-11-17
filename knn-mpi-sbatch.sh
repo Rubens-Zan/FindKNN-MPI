3b1cd065fb5244ffac18bc154e4b9baa2acce1ce
gabdepa <gabriel-r-p-p@hotmail.com>
2023-11-17 10:22:39 -0300

    Changes

--- knn-mpi-sbatch.sh
+++ knn-mpi-sbatch.sh
@@ -11,12 +11,12 @@ make purge && make
 
 # Rodar o programa para APENAS 1 processo MPI e medir o tempo da computaçao de knn
 echo "Rodando sbatch --exclusive para 1 nodo..."
-sbatch --exclusive -N 1 run-knn-mpi 1 
+sbatch --exclusive -N run-knn-mpi.sh 1 
 
 # Rodar o programa para 4 processos MPI no mesmo host e medir o tempo da computaçao de knn
 echo "Rodando sbatch --exclusive para 1 nodo..."
-sbatch --exclusive -N 1 run-knn-mpi 4 
+sbatch --exclusive -N run-knn-mpi.sh 4 
 
 # Rodar o programa para 4 processos MPI em hosts diferentes e medir o tempo da computaçao de knn
 echo "Rodando sbatch --exclusive para 4 nodos..."
-sbatch ./run-knn-mpi 4
+sbatch run-knn-mpi.sh 4
